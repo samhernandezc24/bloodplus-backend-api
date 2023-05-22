dddec6c9c6d109bd2f4505d783a0058b566d3609
samhernandezc24 <samd.hernandezc24@gmail.com>
2023-05-22 10:08:35 -0600

    dummy identation code

--- bloodplus_v1/admin.py
+++ bloodplus_v1/admin.py
@@ -6,9 +6,12 @@ from django.core.exceptions import ValidationError
 
 from .models import User, BloodType
 
+
 class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Confirmar contraseña", widget=forms.PasswordInput)
+    password1 = forms.CharField(
+        label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Confirmar contraseña", widget=forms.PasswordInput)
 
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
@@ -16,14 +19,14 @@ class UserCreationForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise ValidationError("Las contraseñas no coinciden.")
         return password2
-    
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
-    
+
 
 class UserChangeForm(forms.ModelForm):
     """Un formulario para actualizar usuarios. Incluye todos los campos de
@@ -34,10 +37,11 @@ class UserChangeForm(forms.ModelForm):
     class Meta:
         model = User
         fields = [
-            "email", "username", "first_name", "last_name", "password", 
+            "email", "username", "first_name", "last_name", "password",
             "date_of_birth", "gender", "phone", "is_staff", "is_superuser",
             "is_active", "is_subscribed"
         ]
+
 
 class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
@@ -45,11 +49,15 @@ class UserAdmin(BaseUserAdmin):
 
     list_display = [
         'email', 'username', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined']
-    list_filter = ['is_active', 'is_staff', 'gender', 'is_subscribed', 'blood', 'role']
+    list_filter = ['is_active', 'is_staff',
+                   'gender', 'is_subscribed', 'blood', 'role']
     fieldsets = [
-        (None, {"fields": ["email", "username", "first_name", "last_name", "password"]}),
-        ("Información Personal", {"fields": ["date_of_birth", "gender", "phone", "blood"]}),
-        ("Permisos", {"fields": ["role", "is_staff", "is_superuser", "is_active", "is_subscribed"]}),
+        (None, {"fields": ["email", "username",
+         "first_name", "last_name", "password"]}),
+        ("Información Personal", {"fields": [
+         "date_of_birth", "gender", "phone", "blood"]}),
+        ("Permisos", {"fields": ["role", "is_staff",
+         "is_superuser", "is_active", "is_subscribed"]}),
     ]
     # add_fieldsets no es un atributo estándar de ModelAdmin. UserAdmin
     # anula get_fieldsets para usar este atributo cuando se crea un usuario.
@@ -65,16 +73,17 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
         (
-            'Opciones avanzadas', 
+            'Opciones avanzadas',
             {
                 "classes": ["collapse"],
                 "fields": [
-                   "role", "is_staff", "is_superuser", "is_subscribed"],
+                    "role", "is_staff", "is_superuser", "is_subscribed"],
             }
         ),
     ]
     ordering = ['-date_joined']
     filter_horizontal = []
+
 
 admin.site.register(User, UserAdmin)
 admin.site.register(BloodType)
